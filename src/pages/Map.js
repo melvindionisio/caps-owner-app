e5e9405f4ab74e7ce93667f6c15b49640567b1e6
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2021-12-24 23:24:40 +0800

    coordinates update interface

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,5 +1,12 @@
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import { Typography, IconButton, Slide } from "@mui/material";
+import {
+  Typography,
+  IconButton,
+  Slide,
+  TextField,
+  Button,
+  Alert,
+} from "@mui/material";
 import Box from "@mui/material/Box";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import AddIcon from "@mui/icons-material/Add";
@@ -15,6 +22,8 @@ mapboxgl.accessToken =
 
 const Map = () => {
   const controls = new mapboxgl.NavigationControl();
+  const { currentOwner } = useContext(LoginContext);
+
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(124.665);
@@ -22,7 +31,19 @@ const Map = () => {
   const [zoom, setZoom] = useState(16.1);
   const history = useHistory();
 
-  const { currentOwner } = useContext(LoginContext);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [isEditCoordinates, setIsEditCoordinates] = useState(false);
+
+  const [severity, setSeverity] = useState("warning");
+
+  const [alertMessage, setAlertmessage] = useState("");
+  const [showMessageAlert, setShowMessageAlert] = useState(false);
+
+  const [geojson, setGeojson] = useState({
+    type: "FeatureCollection",
+    features: [],
+  });
 
   const BOUNDS = [
     [124.2389, 11.8762], // southwest coordinates
@@ -36,7 +57,9 @@ const Map = () => {
       // darkmode
       // style: "mapbox://styles/mapbox/navigation-night-v1?optimize=true",
       // lightmode
-      style: "mapbox://styles/mapbox/streets-v11",
+      // style: "mapbox://styles/mapbox/streets-v11",
+      style: "mapbox://styles/melsio/ckxh2zv6w0izd14npfm5p9cn5",
+
       // style: "mapbox://styles/mapbox/light-v10?optimize=true",
       center: [lng, lat],
       zoom: zoom,
@@ -60,25 +83,9 @@ const Map = () => {
         return res.json();
       })
       .then((data) => {
-        console.log(data);
-        data.features.forEach(function (marker) {
-          const el = document.createElement("div");
-          el.innerHTML = `<img src="${mapmarker}"/>`;
-          el.className = "marker";
-
-          new mapboxgl.Marker(el)
-            .setLngLat(marker.geometry.coordinates)
-            .setPopup(
-              new mapboxgl.Popup({
-                offset: 20,
-                closeButton: false,
-              })
-                .setHTML(`<h6>&#160; &#160;${marker.properties.title}&#160; &#160;</h6>
-            <h5>${marker.properties.description}</h5>
-           `)
-            )
-            .addTo(map.current);
-        });
+        setGeojson(data);
+        setLongitude(data.features[0].geometry.coordinates[0]);
+        setLatitude(data.features[0].geometry.coordinates[1]);
       })
       .catch((err) => {
         if (err.name === "AbortError") {
@@ -160,6 +167,27 @@ const Map = () => {
   });
 
   useEffect(() => {
+    geojson.features.forEach(function (marker) {
+      const el = document.createElement("div");
+      el.innerHTML = `<img src="${mapmarker}"/>`;
+      el.className = "marker";
+
+      new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(
+          new mapboxgl.Popup({
+            offset: 20,
+            closeButton: false,
+          })
+            .setHTML(`<h6>&#160; &#160;${marker.properties.title}&#160; &#160;</h6>
+        <h5>${marker.properties.description}</h5>
+       `)
+        )
+        .addTo(map.current);
+    });
+  }, [geojson]);
+
+  useEffect(() => {
     if (!map.current) return; // wait for map to initialize
     map.current.on("move", () => {
       setLng(map.current.getCenter().lng.toFixed(4));
@@ -167,6 +195,65 @@ const Map = () => {
       setZoom(map.current.getZoom().toFixed(2));
     });
   });
+
+  const setNewCoordinates = () => {
+    setIsEditCoordinates(!isEditCoordinates);
+    // window.location.reload(false);
+
+    fetch(
+      `http://localhost:3500/api/boarding-houses/owner-map/update-coordinates/${currentOwner.id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          newLongitude: longitude,
+          newLatitude: latitude,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setAlertmessage(data.message);
+        setShowMessageAlert(true);
+        setSeverity("success");
+
+        const abortCont = new AbortController();
+        fetch(
+          `http://localhost:3500/api/boarding-houses/owner-map/map-marks/${currentOwner.id}`,
+          {
+            signal: abortCont.signal,
+          }
+        )
+          .then((res) => {
+            if (!res.ok) {
+              throw Error("Something went wrong!");
+            }
+            return res.json();
+          })
+          .then((data) => {
+            setGeojson(data);
+          })
+          .catch((err) => {
+            if (err.name === "AbortError") {
+              console.log("fetch aborted");
+            } else {
+              console.log("ready");
+            }
+          });
+      });
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (showMessageAlert) {
+        setShowMessageAlert(false);
+      }
+    }, 5000);
+  }, [showMessageAlert]);
 
   return (
     <Box
@@ -181,7 +268,7 @@ const Map = () => {
     >
       <AddIcon
         sx={{
-          color: "#111",
+          color: blue[700],
           zIndex: "1",
           position: "absolute",
           top: "50%",
@@ -190,6 +277,7 @@ const Map = () => {
         }}
         fontSize="large"
       />
+
       <Slide in={true} direction="down">
         <Box
           sx={{
@@ -240,6 +328,91 @@ const Map = () => {
           <Typography variant="caption">
             Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
           </Typography>
+        </Box>
+      </Slide>
+      <Slide in={true} direction="up">
+        <Box
+          sx={{
+            backgroundColor: "rgba(255,255,255, 0.6)",
+            backdropFilter: "blur(1.5rem)",
+            color: "#444",
+            padding: ".6rem",
+            zIndex: "100",
+            position: "absolute",
+            bottom: ".5rem",
+            left: ".5rem",
+            borderRadius: ".5rem",
+            width: 280,
+            boxShadow: "0 0 5px 1px rgba(0,0,0,0.2)",
+          }}
+        >
+          <Typography
+            variant="caption"
+            color="initial"
+            align="center"
+            sx={{ display: "block", mb: 1 }}
+          >
+            My Boarding House Location
+          </Typography>
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <TextField
+              id="longitude"
+              label="Longitude"
+              value={longitude}
+              onChangeCapture={(e) => setLongitude(parseFloat(e.target.value))}
+              size="small"
+              disabled={!isEditCoordinates}
+            />
+            <TextField
+              id="latitude"
+              label="Latitude"
+              value={latitude}
+              onChangeCapture={(e) => setLatitude(parseFloat(e.target.value))}
+              size="small"
+              disabled={!isEditCoordinates}
+            />
+          </Box>
+          {isEditCoordinates ? (
+            <Button
+              variant="contained"
+              size="small"
+              fullWidth
+              color="secondary"
+              sx={{ mt: 1 }}
+              onClick={setNewCoordinates}
+            >
+              Save coordinates
+            </Button>
+          ) : (
+            <>
+              <Button
+                variant="contained"
+                size="small"
+                fullWidth
+                color="secondary"
+                sx={{ mt: 1 }}
+                onClick={() => setIsEditCoordinates(!isEditCoordinates)}
+              >
+                Edit Coordinates
+              </Button>
+              <Alert
+                severity={severity}
+                sx={
+                  showMessageAlert
+                    ? {
+                        display: "flex",
+                        mt: 2,
+                      }
+                    : {
+                        display: "none",
+                        mt: 2,
+                      }
+                }
+              >
+                {alertMessage}
+              </Alert>
+            </>
+          )}
         </Box>
       </Slide>
     </Box>
