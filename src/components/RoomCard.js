c14f076e6f39b0fe0bac842aff4bb6f771b8fb84
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2022-01-05 22:01:16 +0800

    medyo ok na rooms What the fuck?

--- src/components/RoomCard.js
+++ src/components/RoomCard.js
@@ -1,97 +1,145 @@
 import React from "react";
 import {
-  Card,
-  CardHeader,
-  IconButton,
-  Switch,
-  FormGroup,
-  FormControlLabel,
-  Typography,
-  Grid,
-  Box,
-  Button,
-  Divider,
+    Card,
+    CardHeader,
+    IconButton,
+    Switch,
+    FormGroup,
+    FormControlLabel,
+    Typography,
+    Grid,
+    Box,
+    Button,
+    Divider,
 } from "@mui/material";
 import { useState } from "react";
-import { grey } from "@mui/material/colors";
+import { grey, blue } from "@mui/material/colors";
 
-const RoomCard = () => {
-  const [isAvailable, setIsAvailable] = useState(true);
-  const handleAvailabilityChange = () => {
-    setIsAvailable(!isAvailable);
-    // This is where every change sends change to the database.
-  };
-  return (
-    <Card sx={{ borderRadius: 2 }}>
-      <CardHeader
-        action={<IconButton aria-label=""></IconButton>}
-        title="Room Name"
-        sx={{ marginBottom: 0, paddingBottom: 1 }}
-      />
-      <Box sx={{ p: 1 }}>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <Box sx={{ px: 1 }}>
-              <Typography variant="subtitle2" color="text.secondary">
-                Total Bed
-              </Typography>
-              <Typography variant="subtitle2" color="text.secondary">
-                Available Bed
-              </Typography>
-              <Typography
-                variant="subtitle2"
-                color="text.secondary"
-                marginBottom
-              >
-                Occupied Bed
-              </Typography>
-              <Divider />
-            </Box>
-          </Grid>
-          <Grid item xs={12}>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "flex-end",
-
-                borderRadius: 1,
-                background: grey[100],
-                height: "100%",
-              }}
-            >
-              <FormGroup>
-                <FormControlLabel
-                  sx={{ color: "text.secondary" }}
-                  label={isAvailable ? "ENABLED" : "DISABLED"}
-                  labelPlacement="left"
-                  value="bottom"
-                  control={
-                    <Switch
-                      size="medium"
-                      color="secondary"
-                      checked={isAvailable}
-                      onChange={handleAvailabilityChange}
-                    />
-                  }
+const RoomCard = ({ room }) => {
+    const [isAvailable, setIsAvailable] = useState(true);
+    const handleAvailabilityChange = () => {
+        setIsAvailable(!isAvailable);
+        // This is where every change sends change to the database.
+    };
+    return (
+        <Grid item lg={3} xs={12} md={4} sm={6}>
+            <Card sx={{ borderRadius: 2 }}>
+                <CardHeader
+                    action={<IconButton aria-label=""></IconButton>}
+                    title={
+                        <Typography
+                            variant="h6"
+                            color="initial"
+                            align="center"
+                            sx={{ textTransform: "uppercase", fontSize: 22 }}
+                        >
+                            {room.name}
+                        </Typography>
+                    }
+                    sx={{ marginBottom: 0, paddingBottom: 1 }}
                 />
-              </FormGroup>
-            </Box>
-          </Grid>
+                <Box sx={{ p: 1, pt: 0 }}>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12}>
+                            <Box sx={{ px: 1 }}>
+                                <Typography
+                                    variant="subtitle2"
+                                    color="text.secondary"
+                                >
+                                    Total Bed:{" "}
+                                    <span
+                                        style={{
+                                            fontWeight: "bold",
+                                            fontSize: 18,
+                                            color: blue[500],
+                                        }}
+                                    >
+                                        {room.numSlots}
+                                    </span>
+                                </Typography>
+                                <Typography
+                                    variant="subtitle2"
+                                    color="text.secondary"
+                                >
+                                    Available Bed:{" "}
+                                    <span
+                                        style={{
+                                            fontWeight: "bold",
+                                            fontSize: 18,
+                                            color: blue[500],
+                                        }}
+                                    >
+                                        {room.availableSlots}
+                                    </span>
+                                </Typography>
+                                <Typography
+                                    variant="subtitle2"
+                                    color="text.secondary"
+                                    marginBottom
+                                >
+                                    Occupied Bed:{" "}
+                                    <span
+                                        style={{
+                                            fontWeight: "bold",
+                                            fontSize: 18,
+                                            color: blue[500],
+                                        }}
+                                    >
+                                        {room.numSlots - room.availableSlots}
+                                    </span>
+                                </Typography>
+                                <Divider />
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "flex-end",
+
+                                    borderRadius: 1,
+                                    background: grey[100],
+                                    height: "100%",
+                                }}
+                            >
+                                <FormGroup>
+                                    <FormControlLabel
+                                        sx={{ color: "text.secondary" }}
+                                        label={
+                                            isAvailable ? "ENABLED" : "DISABLED"
+                                        }
+                                        labelPlacement="end"
+                                        value="bottom"
+                                        control={
+                                            <Switch
+                                                size="medium"
+                                                color="secondary"
+                                                checked={isAvailable}
+                                                onChange={
+                                                    handleAvailabilityChange
+                                                }
+                                            />
+                                        }
+                                    />
+                                </FormGroup>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Box>
+                <Button
+                    disableElevation
+                    fullWidth
+                    variant="contained"
+                    size="medium"
+                    color="secondary"
+                    sx={{ mt: 1, borderRadius: 0 }}
+                >
+                    View Room
+                </Button>
+            </Card>
         </Grid>
-      </Box>
-      <Button
-        disableElevation
-        fullWidth
-        variant="contained"
-        size="medium"
-        color="secondary"
-        sx={{ mt: 1, borderRadius: 0 }}
-      >
-        View Room
-      </Button>
-    </Card>
-  );
+    );
 };
 
 export default RoomCard;
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,42 +3,42 @@
 import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const abortCont = new AbortController();
+    useEffect(() => {
+        const abortCont = new AbortController();
 
-    setTimeout(() => {
-      fetch(url, { signal: abortCont.signal })
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Something went wrong!");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setData(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          if (err.name === "AbortError") {
-            console.log("fetch aborted");
-          } else {
-            setIsPending(false);
-            setError(err.message);
-            setData(null);
-          }
-        });
-    }, 0);
-    return () => {
-      abortCont.abort();
-    };
-  }, [url]); //useEffect dependency
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+                .then((res) => {
+                    if (!res.ok) {
+                        throw Error("Something went wrong!");
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    if (err.name === "AbortError") {
+                        console.log("fetch aborted");
+                    } else {
+                        setIsPending(false);
+                        setError(err.message);
+                        setData(null);
+                    }
+                });
+        }, 0);
+        return () => {
+            abortCont.abort();
+        };
+    }, [url]); //useEffect dependency
 
-  return { data, isPending, error };
+    return { data, isPending, error };
 };
 
 export default useFetch;
